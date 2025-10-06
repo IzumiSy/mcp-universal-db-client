b85d3c3212fe133c51a2d8eb891ce2f41ddab8de
IzumiSy <beetle-noise@gmx.com>
2025-10-07 01:23:10 +0900

    Enable minification in production build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,11 @@
-import { defineConfig } from "tsdown";
+import { defineConfig, UserConfig } from "tsdown";
+
+const opts: UserConfig =
+  process.env.NODE_ENV === "production"
+    ? { minify: true }
+    : { sourcemap: true };
 
 export default defineConfig({
   entry: ["./src/index.ts"],
+  ...opts,
 });
