8293e435e7f31358bc40b64bc3f2f5a57e3ec673
IzumiSy <beetle-noise@gmx.com>
2025-12-31 13:25:01 +0900

    Fix dist files

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
+  entry: "./src/index.ts",
   ...(process.env.NODE_ENV === "production"
     ? { minify: true }
-    : { sourcemap: true, hoge: "aaa" }),
+    : { sourcemap: true }),
 });
